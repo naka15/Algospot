fb604a26c0b205e51d9b1b91ce7b585e0f31d2f7
Jae-Hyuk Choi <nakaa15@naver.com>
2015-07-03 13:55:07 +0900

    add mercy

---
+++ 01.Mercy/src/Mercy.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Mercy {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+		int i=0;
+		while (i<cases) {
+			System.out.println("Hello Algospot!");
+			i++;
+		}
+	}
+}
